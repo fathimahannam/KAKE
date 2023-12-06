2b55e264189f5084a3bfeebea3a6fff44f5c75dd
fathimahannam <122714714+fathimahannam@users.noreply.github.com>
2023-12-06 14:52:24 +0400

    changed to sql

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -177,23 +177,23 @@ ASGI_APPLICATION = 'myproject.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kake',
-        'USER':'postgres',
-        'PASSWORD':'hanna',
-        'HOST':'Localhost'
-
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kake',
+#         'USER':'postgres',
+#         'PASSWORD':'hanna',
+#         'HOST':'Localhost'
+
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
